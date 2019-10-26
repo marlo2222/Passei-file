19a0b7018c112da759849d2485a880ade0edc348
marlo2222 <marlo_henrique@hotmail.com.br>
2019-10-26 14:06:46 -0300

    mudando o repository

--- src/main/java/com/create/file/controller/DocumentoController.java
+++ src/main/java/com/create/file/controller/DocumentoController.java
@@ -34,6 +34,8 @@ public class DocumentoController {
 	@RequestMapping(value = "documento/adicionar/",method = RequestMethod.POST)
 	@ApiOperation(value = "Recebe um ou varios arquivos")
 	public ResponseEntity<?> addDocumento(@RequestParam("arquivo")MultipartFile[] file) throws IOException, NoSuchAlgorithmException {
+		System.out.println("File Name: " + file.getOriginalFilename());
+        System.out.println("File Content Type: " + file.getContentType());
 		return documentoService.addDocumentos(file);
 	}
 
